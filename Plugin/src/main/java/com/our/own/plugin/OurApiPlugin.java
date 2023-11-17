98fa14dc17064b29cbbbfd84e7bccb914a245d31
John Kellner <john.kellner83@googlemail.com>
2023-11-17 13:23:55 +0100

    Gradle Plugin

---
+++ Plugin/src/main/java/com/our/own/plugin/OurApiPlugin.java
@@ -0,0 +1,12 @@
+package com.our.own.plugin;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class OurApiPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project target) {
+
+    }
+}
